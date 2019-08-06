1c85ed0f64b77211acb643fcbd6a676279e23497
Ethan Liu <liuyuxuan1230@outlook.com>
2019-08-06 15:30:10 +0800

    Creating Interfaces for service, closes #5.

---
+++ pet-clinic-data/src/main/java/guru/spring/petclinic/services/PetService.java
@@ -0,0 +1,15 @@
+package guru.spring.petclinic.services;
+
+import guru.spring.petclinic.model.Pet;
+
+import java.util.Set;
+
+public interface PetService {
+
+    Pet findById(Long id);
+
+    Pet save(Pet owner);
+
+    Set<Pet> findAll();
+
+}
