2029c6e722ef6cfb86d6d5ceee6476bf5f595d60
Ethan Liu <liuyuxuan1230@outlook.com>
2019-08-07 11:04:21 +0800

    refactoring services to crudService, closes #11.

--- pet-clinic-data/src/main/java/guru/spring/petclinic/services/VetService.java
+++ pet-clinic-data/src/main/java/guru/spring/petclinic/services/VetService.java
@@ -2,13 +2,5 @@ package guru.spring.petclinic.services;
 
 import guru.spring.petclinic.model.Vet;
 
-import java.util.Set;
-
-public interface VetService {
-
-    Vet findById(Long id);
-
-    Vet save(Vet owner);
-
-    Set<Vet> findAll();
+public interface VetService extends CrudService<Vet, Long> {
 }
